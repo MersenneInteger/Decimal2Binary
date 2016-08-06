3a3cb78b668c739f17b2387296429ff9026c4861
MersenneInteger <aUtomata2.71828@openmailbox.org>
2016-08-06 07:37:24 -0500

    working for >512

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Decimal2Binary
+This program converts an integer passed as a command line argument
+and converts it to binary. 
--- decimal2binary.c
+++ decimal2binary.c
@@ -3,18 +3,17 @@
 #include <string.h>
 
 void switchOne(int array[], int size);
-//void initializeArraySize(int array[], size_t size);
     
 int main(int argc, char *argv[]){
 
     int decNumber = atoi(argv[1]);
-    int numLength = strlen(argv[1]);
     const int binSize8 = 8, binSize16 = 16;
     const int byteSize = 256;
     const size_t size8 = 8, size16 = 16;
     int counter = 0;
     
     if (decNumber < byteSize){
+
         int *numList = calloc(size8, sizeof(int));
         int binList[] = {128,64,32,16,8,4,2,1};
 
@@ -25,23 +24,25 @@ int main(int argc, char *argv[]){
         switchOne(numList, binSize8);
         for(int i = 0; i < binSize8; i++)
             printf("%d", numList[i]);
-    }/*
-    else if (decNumber >= byteSize){
-        int *numList = calloc(size8, sizeof(int)); 
-        int binList[] = {256,128,64,32,16,8,4,2,1};
-        
-        while (counter < 9){
+    }
+    else if (decNumber >= byteSize){    
+
+        int *numList = calloc(size16, sizeof(int)); 
+        int binList[] = {1,2,4,8,16,32,64,128,256};
+
+        while (counter < (binSize8+1)){
             numList[counter] = (decNumber & binList[counter]);
             counter++;
         }
         switchOne(numList, binSize16);
         for(int i = binSize16-1; i >= 0; i--)
             printf("%d", numList[i]); 
-    }*/
-    else{/*
-        printf("Error invalid input");
-        return 1;*/
     }
+    else if(decNumber < 1){
+        printf("Error invalid input, enter positive integer.");
+        return 1;
+    }
+    else{ printf("Error invalid input\n"); }
     printf("\n");
 }//end of main
 
